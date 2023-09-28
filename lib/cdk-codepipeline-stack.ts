76b210aa7cf95f363f6e326293a0a667865dd8fb
Afshin <fallahi_a@yahoo.com>
2023-09-28 16:54:08 -0400

    lambda init

--- lib/cdk-codepipeline-stack.ts
+++ lib/cdk-codepipeline-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from "./stage";
 
 export class CdkCodepipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'CdkPipeline', {
+    const pipeline = new CodePipeline(this, 'CdkPipeline', {
       pipelineName: 'my-pipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('afallahi/cdk-codepipeline', 'cdk-init'),
@@ -17,6 +18,17 @@ export class CdkCodepipelineStack extends cdk.Stack {
         ],
       }),
     });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "558532633158", region: "us-east-1" }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep("Manual approval before production"));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "558532633158", region: "us-east-1" }
+    }));
+
 
   }
 }
